8f6fac8eaed6bee1fa400b18a33494450f9c191b
Shira Atkins <shira0556786280@gmail.com>
2025-05-01 16:33:22 +0300

    change index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const todosRouter = require("./routers/todosRouter")
-const guideStepRouter = require("./routers/guideStepRouter")
+const todosRouter = require("./routers/todosRouter");
+const guideStepRouter = require("./routers/guideStepRouter");
 const eventRouter = require("./routers/eventRouter");
-const listItemRouter = require("./routers/listItemRouter")
-const e = require('express');
-
+const listItemRouter = require("./routers/listItemRouter");
 
 require('dotenv').config();
 
@@ -15,16 +13,11 @@ const PORT = process.env.PORT || 8080;
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use("/todos", todosRouter)
-app.use("/guideStep", require(guideStepRouter));
-app.use("/event", require(eventRouter));
-app.use("/listItem", require(listItemRouter));
+app.use("/todos", todosRouter);
+app.use("/guideStep", guideStepRouter);
+app.use("/event", eventRouter);
+app.use("/listItem", listItemRouter);
 app.use("/uploads", express.static("uploads"));
-
-
-// Routes
-
-
 
 // Start the server
 app.listen(PORT, () => {
